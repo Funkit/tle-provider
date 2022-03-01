6e119ba6d1101638da686a5bcb6f800ba852a33c
camille.chomel <camille.chomel@FOXE-FR>
2022-03-01 17:25:51 +0100

    add getdata function body

--- data/postgresql.go
+++ data/postgresql.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -14,6 +15,15 @@ import (
 type PostgreSQLClient struct {
 	db           *bun.DB
 	ServerConfig map[string]interface{}
+}
+
+type pgSatData struct {
+	bun.BaseModel `bun:"table:satellite-tle-data,alias:tle"`
+
+	noradID       int `bun:",pk"`
+	satelliteName string
+	tleLine1      string
+	tleLine2      string
 }
 
 // NewPostgreSQLClient Generates a new PostgreSQL client from the information in the configuration file
@@ -53,6 +63,22 @@ func (pgc *PostgreSQLClient) GetConfig() (map[string]interface{}, error) {
 }
 
 // GetData Implementation of the Source interface for Celestrak
-func (pgc *PostgreSQLClient) GetData() ([]Satellite, error) {
-	return []Satellite{}, nil
+func (pgc *PostgreSQLClient) GetData() (satList []Satellite, err error) {
+
+	var data []pgSatData
+	var ctx context.Context
+	if err = pgc.db.NewSelect().Model(data).Scan(ctx); err != nil {
+		return nil, err
+	}
+
+	for _, sat := range data {
+		satList = append(satList, Satellite{
+			SatelliteName: sat.satelliteName,
+			NORADID:       sat.noradID,
+			TLELine1:      sat.tleLine1,
+			TLELine2:      sat.tleLine2,
+		})
+	}
+
+	return satList, nil
 }
